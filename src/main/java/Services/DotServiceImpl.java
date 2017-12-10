3086aab8abc0716da3870b18b52fdeaf688b0afc
SlyF0Xx <max.tolstukhin@mail.ru>
2017-12-10 20:40:54 +0300

    Add @Stateless behaviour to the DotService

--- src/main/java/Services/DotServiceImpl.java
+++ src/main/java/Services/DotServiceImpl.java
@@ -7,8 +7,10 @@ import Areas.impl.TriangleArea;
 import Services.DotService;
 
 import javax.ejb.Local;
+import javax.ejb.Stateless;
 
 @Local
+@Stateless
 public class DotServiceImpl extends DotService<Integer,Double,Integer> {
 
     public DotServiceImpl() {
