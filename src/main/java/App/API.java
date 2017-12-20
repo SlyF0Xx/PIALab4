9c57897a34242107a45d6488d6cea5ec721d092c
SlyF0Xx <max.tolstukhin@mail.ru>
2017-12-20 04:03:28 +0300

    Add core endpoints

--- src/main/java/App/API.java
+++ src/main/java/App/API.java
@@ -1,24 +1,141 @@
 package App;
 
+import Services.DotService;
 import Services.DotServiceImpl;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import model.Dot;
+import model.User;
 
 import javax.ejb.*;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import java.util.List;
 
-@Lock(LockType.WRITE)
-@Produces(MediaType.APPLICATION_XML)
+@Path("/API")
+@Stateless
 public class API {
-    @EJB
-    DotServiceImpl DotService;
+    DotService dotService = new DotServiceImpl();
 
-    @Path("/shoot")
+    /*private static final EntityManagerFactory emFactoryObj;
+    private static final String PERSISTENCE_UNIT_NAME = "NewPersistenceUnit";
+
+    static {
+        emFactoryObj = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+    }
+        // This Method Is Used To Retrieve The 'EntityManager' Object
+    public EntityManager getEntityManager() {
+        return emFactoryObj.createEntityManager();
+    }*/
+
+    @PersistenceContext(unitName = "NewPersistenceUnit")
+    EntityManager entityManager;
+
+    @Path("/login")
+    @POST
+    @Transactional
+    public void login(@FormParam("name") String name,
+                      @FormParam("password") String password,
+                      @Context HttpServletRequest req) {
+        //EntityManager entityManager = getEntityManager();
+
+        List<User> userList = entityManager.createQuery("SELECT user FROM User user WHERE user.name = :name")
+                .setParameter("name", name)
+                .getResultList();
+
+        if(userList.size() == 1 && userList.get(0).getPassword() == password.hashCode() )
+        {
+            req.getSession().setAttribute("id", userList.get(0).getId());
+        }
+    }
+
+    @Path("/register")
+    @POST
+    @Transactional
+    public void register(@FormParam("name") String name,
+                         @FormParam("password") String password,
+                         @Context HttpServletRequest req) {
+        User user = new User(name, password.hashCode());
+        //EntityManager entityManager = getEntityManager();
+        entityManager.persist(user);
+        entityManager.flush();
+        req.getSession().setAttribute("id", user.getId()+1);
+    }
+
+    @Path("/shoot/{x}/{y}/{size}")
+    @GET
+    //@Produces(value = {"text/xml", "application/json"})
+    @Transactional
+    public String shoot(@PathParam("x") Integer x,
+                         @PathParam("y") Double y,
+                         @PathParam("size") Integer size,
+                         @Context HttpServletRequest req) {
+        //EntityManager entityManager = getEntityManager();
+        List<User> user = entityManager.createQuery("SELECT user FROM User user where user.id = :ident")
+                .setParameter("ident",req.getSession().getAttribute("id")).getResultList();
+
+        if(user.size() == 1)
+        {
+            Dot dot = new Dot(x, y, size, dotService.isIn(x, y, size), user.get(0));
+            entityManager.persist(dot);
+
+            return dot.getShoot().toString();
+        }
+        else {
+            return "false";
+        }
+    }
+
+    @Path("/existName/{name}")
+    @GET
+    @Produces(value = {"text/xml", "application/json"})
+    @Transactional
+    public boolean existName(@PathParam("name") String name) {
+        //EntityManager entityManager = getEntityManager();
+        List<Integer> list = entityManager.createQuery("SELECT COUNT(user) FROM User user where user.name = :name")
+                .setParameter("name", name).getResultList();
+
+        return list.get(0) == 1;
+    }
+
+    @Path("/logout")
+    @GET
+    public void logout(@Context HttpServletRequest req) {
+        req.getSession().setAttribute("id", 0);
+    }
+
+
+    @Path("/list")
     @GET
     @Produces(value={"text/xml", "application/json"})
-    public Boolean shoot() {
-        return DotService.isIn(1, 1.0, 4);
+    @Transactional
+    public String list(@Context HttpServletRequest req) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        //EntityManager entityManager = getEntityManager();
+        if(req.getSession().getAttribute("id") != null)
+        {
+            List<Dot> dots = entityManager.createQuery("SELECT d FROM Dot d WHERE d.user.id = :ident")
+                    .setParameter("ident", 1)
+                    .getResultList();
+
+            return objectMapper.writeValueAsString(dots);
+
+        }
+        return "";
+        /*
+         List<User> user = entityManager.createQuery("SELECT user FROM User user WHERE user.id = :id")
+                .setParameter("id", req.getSession().getAttribute("id"))
+                .getResultList();
+
+        return objectMapper.writeValueAsString(user.get(0).getDots());
+         */
     }
 
 }
--- web/index.jsp
+++ web/index.jsp
@@ -1,6 +1,6 @@
 <%--
   Created by IntelliJ IDEA.
-  User: SlyFox
+  model.User: SlyFox
   Date: 09.12.2017
   Time: 22:04
   To change this template use File | Settings | File Templates.
@@ -10,8 +10,22 @@
 <html>
   <body>
 
+
+
+  <form action="/untitled_war/rest/API/login" method="post">
+    <input id="name" name="name">
+    <input type="password" id="password" name="password">
+    <input type="submit">
+  </form>
+
+  <form action="/untitled_war/rest/API/register" method="post">
+    <input name="name">
+    <input type="password" name="password">
+    <input type="submit">
+  </form>
   <%
     out.print("Ou");
+
 
 /*    DotServiceImpl dotService =(DotServiceImpl)session.getAttribute("service");
     if (dotService.isIn(1, 1.0, 4)) {
